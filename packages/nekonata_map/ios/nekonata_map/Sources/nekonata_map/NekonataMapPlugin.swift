05ee0f2770fd4a959569b649042a346ad93d95d1
tyPhoon <typhoonmoon1022@gmail.com>
2025-03-10 01:28:17 +0900

    Fix:NekonataMapPluginでのgestureRecognizersBlockingPolicyを追加

--- packages/nekonata_map/ios/nekonata_map/Sources/nekonata_map/NekonataMapPlugin.swift
+++ packages/nekonata_map/ios/nekonata_map/Sources/nekonata_map/NekonataMapPlugin.swift
@@ -4,6 +4,9 @@ import UIKit
 public class NekonataMapPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let factory = NekonataMapViewFactory(messenger: registrar.messenger())
-    registrar.register(factory, withId: "nekonata_map")
+    registrar.register(
+      factory, withId: "nekonata_map",
+      gestureRecognizersBlockingPolicy:
+        FlutterPlatformViewGestureRecognizersBlockingPolicyWaitUntilTouchesEnded)
   }
 }
