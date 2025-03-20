79f2a856df21ef7f3a5bd394a4cd5b76a5bd83c1
tyPhoon <typhoonmoon1022@gmail.com>
2025-03-21 01:57:44 +0900

    Fix:swiftのファイル群を適切に紐づけ

---
+++ packages/nekonata_location_fetcher/ios/nekonata_location_fetcher/Sources/nekonata_location_fetcher/Session.swift
@@ -0,0 +1,17 @@
+import CoreLocation
+
+@available(iOS 17.0, *)
+class BackgroundActivitySessionManager {
+    static private var backgroundActivitySession: CLBackgroundActivitySession?
+
+    public static func activate() {
+        backgroundActivitySession = CLBackgroundActivitySession()
+    }
+
+    public static func invalidate() {
+        if let session = backgroundActivitySession {
+            session.invalidate()
+            backgroundActivitySession = nil
+        }
+    }
+}
