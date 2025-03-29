8305839c553e09f953e9ee9005f085cc668c549b
tyPhoon <typhoonmoon1022@gmail.com>
2025-03-30 00:34:58 +0900

    Fix: setRegionの潜在的なバグの修正とコードのフォーマット

--- packages/nekonata_map/ios/nekonata_map/Sources/nekonata_map/NekonataMapView.swift
+++ packages/nekonata_map/ios/nekonata_map/Sources/nekonata_map/NekonataMapView.swift
@@ -31,12 +31,12 @@ class NekonataMapViewFactory: NSObject, FlutterPlatformViewFactory {
 class NekonataMapView: NSObject, FlutterPlatformView {
     private var mapView: MKMapView
     private var channel: FlutterMethodChannel
-    
+
     private var initialCoordinates: CLLocationCoordinate2D?
 
     private var regionDidChangeWorkItem: DispatchWorkItem?
     private var preZoomLevel: Double?
-    
+
     private var baseDistance: Double = 0
     private var setUpFinished: Bool = false
 
@@ -46,7 +46,8 @@ class NekonataMapView: NSObject, FlutterPlatformView {
         arguments args: Any?,
         binaryMessenger messenger: FlutterBinaryMessenger?
     ) {
-        channel = FlutterMethodChannel(name: "nekonata_map_\(viewId)", binaryMessenger: messenger!)
+        channel = FlutterMethodChannel(
+            name: "nekonata_map_\(viewId)", binaryMessenger: messenger!)
         mapView = MKMapView(frame: frame)
 
         super.init()
@@ -62,9 +63,11 @@ class NekonataMapView: NSObject, FlutterPlatformView {
             let latitude = dict["latitude"] as? CLLocationDegrees,
             let longitude = dict["longitude"] as? CLLocationDegrees
         {
-            initialCoordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            initialCoordinates = CLLocationCoordinate2D(
+                latitude: latitude, longitude: longitude)
         }
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleMapTap(_:)))
+        let tapGesture = UITapGestureRecognizer(
+            target: self, action: #selector(handleMapTap(_:)))
         tapGesture.delegate = self
         tapGesture.cancelsTouchesInView = false
         mapView.addGestureRecognizer(tapGesture)
@@ -77,7 +80,9 @@ class NekonataMapView: NSObject, FlutterPlatformView {
         return mapView
     }
 
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    public func handle(
+        _ call: FlutterMethodCall, result: @escaping FlutterResult
+    ) {
         DispatchQueue.main.async {
             switch call.method {
             case "addMarker":
@@ -115,9 +120,12 @@ class NekonataMapView: NSObject, FlutterPlatformView {
             return
         }
 
-        let annotation = Annotation(id, latitude: latitude, longitude: longitude)
+        let annotation = Annotation(
+            id, latitude: latitude, longitude: longitude)
         // [Platform-specific code | Flutter](https://docs.flutter.dev/platform-integration/platform-channels#codec)
-        annotation.image = (args["image"] as? FlutterStandardTypedData).map { value in value.data }
+        annotation.image = (args["image"] as? FlutterStandardTypedData).map {
+            value in value.data
+        }
         annotation.minWidth = args["minWidth"] as? CGFloat
         annotation.minHeight = args["minHeight"] as? CGFloat
 
@@ -144,7 +152,8 @@ class NekonataMapView: NSObject, FlutterPlatformView {
         }
 
         if let annotation = annotation(withId: id) {
-            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let coordinate = CLLocationCoordinate2D(
+                latitude: latitude, longitude: longitude)
             UIView.animate(withDuration: 0.25) {
                 annotation.coordinate = coordinate
             }
@@ -167,42 +176,47 @@ class NekonataMapView: NSObject, FlutterPlatformView {
             }
         }
     }
-    
+
     func moveCamera(_ call: FlutterMethodCall) {
         guard let args = call.arguments as? [String: Any] else { return }
-        
+
         let current = mapView.camera
-        let latitude = args["latitude"] as? Double ?? current.centerCoordinate.latitude
-        let longitude = args["longitude"] as? Double ?? current.centerCoordinate.longitude
+        let latitude =
+            args["latitude"] as? Double ?? current.centerCoordinate.latitude
+        let longitude =
+            args["longitude"] as? Double ?? current.centerCoordinate.longitude
         let zoom = args["zoom"] as? Double ?? zoomLevel()
         let heading = args["heading"] as? Double ?? current.heading
-        
-        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        
+
+        let coordinate = CLLocationCoordinate2D(
+            latitude: latitude, longitude: longitude)
+
         // ズームレベルに応じた距離（赤道での理想値）
         let unadjustedDistance = baseDistance / pow(2, zoom - 2)
-        
+
         // 中心座標の緯度による補正
         let latFactor = cos(coordinate.latitude * .pi / 180)
         let targetDistanceWithLatitude = unadjustedDistance * latFactor
-        
+
         // 画面回転によるバウンディングボックスの補正
         // fromDistanceは見える範囲に影響しているらしく、回転時には見える範囲が変わるため係数を掛ける必要がある
         let mapSize = mapView.bounds.size
         let originalWidth = Double(mapSize.width)
         let originalHeight = Double(mapSize.height)
         let angle = heading * .pi / 180.0
-        let rotatedWidth = abs(cos(angle)) * originalWidth + abs(sin(angle)) * originalHeight
+        let rotatedWidth =
+            abs(cos(angle)) * originalWidth + abs(sin(angle)) * originalHeight
         let correctionFactor = originalWidth / rotatedWidth
         let adjustedDistance = targetDistanceWithLatitude * correctionFactor
-        
-        let camera = MKMapCamera(lookingAtCenter: coordinate,
-                                 fromDistance: adjustedDistance,
-                                 pitch: current.pitch,
-                                 heading: heading)
+
+        let camera = MKMapCamera(
+            lookingAtCenter: coordinate,
+            fromDistance: adjustedDistance,
+            pitch: current.pitch,
+            heading: heading)
         mapView.setCamera(camera, animated: true)
     }
-    
+
     func setRegion(_ call: FlutterMethodCall) {
         guard let args = call.arguments as? [String: Any],
             let minLat = args["minLatitude"] as? Double,
@@ -216,27 +230,38 @@ class NekonataMapView: NSObject, FlutterPlatformView {
         let centerLon = (minLon + maxLon) / 2
 
         // 緯度・経度の範囲を計算
-        let latDelta = maxLat - minLat
-        let lonDelta = maxLon - minLon
+        var latDelta = maxLat - minLat
+        var lonDelta = maxLon - minLon
+
+        // 過剰な値を制限
+        let maxDelta: Double = 180.0
+        latDelta = min(latDelta, maxDelta)
+        lonDelta = min(lonDelta, maxDelta)
 
         // マップのサイズ取得
         let mapWidth = mapView.bounds.size.width
         let mapHeight = mapView.bounds.size.height
+
+        // マップの幅または高さがゼロの場合は処理を中断
+        guard mapWidth > 0, mapHeight > 0 else { return }
 
         // ピクセルベースのパディングを緯度・経度の割合に変換
         let latPadding = (paddingPx / mapHeight) * latDelta
         let lonPadding = (paddingPx / mapWidth) * lonDelta
 
         let span = MKCoordinateSpan(
-            latitudeDelta: latDelta + latPadding, longitudeDelta: lonDelta + lonPadding)
+            latitudeDelta: latDelta + latPadding,
+            longitudeDelta: lonDelta + lonPadding)
         let region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: centerLat, longitude: centerLon), span: span)
+            center: CLLocationCoordinate2D(
+                latitude: centerLat, longitude: centerLon), span: span)
 
         mapView.setRegion(region, animated: true)
     }
 
     func annotation(withId id: String) -> Annotation? {
-        return mapView.annotations.compactMap({ $0 as? Annotation }).first(where: { $0.id == id })
+        return mapView.annotations.compactMap({ $0 as? Annotation }).first(
+            where: { $0.id == id })
     }
 
     func zoomLevel() -> Double {
@@ -251,42 +276,49 @@ class NekonataMapView: NSObject, FlutterPlatformView {
 extension NekonataMapView: MKMapViewDelegate {
     func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
         guard !setUpFinished else { return }
-        
+
         let baseCoordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
         mapView.setCenter(baseCoordinate, zoomLevel: 2, animated: false)
-        
+
         if #available(iOS 13.0, *) {
             baseDistance = mapView.camera.centerCoordinateDistance
         } else {
             baseDistance = mapView.camera.altitude
         }
-        
-//        debugPrint(baseDistance)
-        
+
+        //        debugPrint(baseDistance)
+
         if let coordinate = initialCoordinates {
             let region = MKCoordinateRegion(
-                center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                center: coordinate, latitudinalMeters: 1000,
+                longitudinalMeters: 1000)
             mapView.setRegion(region, animated: false)
         }
-        
+
         setUpFinished = true
-        
+
         channel.invokeMethod("onMapReady", arguments: nil)
     }
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation)
+        -> MKAnnotationView?
+    {
         guard let annotation = annotation as? Annotation else {
             return nil
         }
 
-        if let view = mapView.dequeueReusableAnnotationView(withIdentifier: annotation.id) {
+        if let view = mapView.dequeueReusableAnnotationView(
+            withIdentifier: annotation.id)
+        {
             return view
         }
 
         if annotation.image != nil {
-            return ImageAnnotationView(annotation: annotation, reuseIdentifier: annotation.id)
+            return ImageAnnotationView(
+                annotation: annotation, reuseIdentifier: annotation.id)
         } else {
-            return MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: annotation.id)
+            return MKMarkerAnnotationView(
+                annotation: annotation, reuseIdentifier: annotation.id)
         }
     }
 
@@ -308,10 +340,10 @@ extension NekonataMapView: MKMapViewDelegate {
             if preZoomLevel != zoomLevel {
                 self.channel.invokeMethod("onZoomEnd", arguments: zoomLevel)
                 preZoomLevel = zoomLevel
-                
-//                if #available(iOS 13.0, *) {
-//                    debugPrint(mapView.camera.centerCoordinateDistance)
-//                }
+
+                //                if #available(iOS 13.0, *) {
+                //                    debugPrint(mapView.camera.centerCoordinateDistance)
+                //                }
             }
         }
 
@@ -323,7 +355,9 @@ extension NekonataMapView: MKMapViewDelegate {
 }
 
 extension NekonataMapView: UIGestureRecognizerDelegate {
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch)
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch
+    )
         -> Bool
     {
         !(touch.view is MKAnnotationView)
@@ -331,7 +365,8 @@ extension NekonataMapView: UIGestureRecognizerDelegate {
 
     func gestureRecognizer(
         _ gestureRecognizer: UIGestureRecognizer,
-        shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+        shouldRecognizeSimultaneouslyWith otherGestureRecognizer:
+            UIGestureRecognizer
     ) -> Bool {
         true
     }
@@ -351,11 +386,14 @@ extension NekonataMapView {
 }
 
 extension MKMapView {
-   func setCenter(_ coordinate: CLLocationCoordinate2D, zoomLevel: Double, animated: Bool) {
-       // ズームレベルに基づいて緯度デルタを計算
-       let span = MKCoordinateSpan(latitudeDelta: 360 / pow(2, zoomLevel),
-                                   longitudeDelta: 360 / pow(2, zoomLevel))
-       let region = MKCoordinateRegion(center: coordinate, span: span)
-       setRegion(region, animated: animated)
-   }
+    func setCenter(
+        _ coordinate: CLLocationCoordinate2D, zoomLevel: Double, animated: Bool
+    ) {
+        // ズームレベルに基づいて緯度デルタを計算
+        let span = MKCoordinateSpan(
+            latitudeDelta: 360 / pow(2, zoomLevel),
+            longitudeDelta: 360 / pow(2, zoomLevel))
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        setRegion(region, animated: animated)
+    }
 }
