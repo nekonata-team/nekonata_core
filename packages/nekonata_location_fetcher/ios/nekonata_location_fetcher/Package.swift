192f840996ce80912986d0ce0aaa603030f9c909
tyPhoon <typhoonmoon1022@gmail.com>
2026-02-10 23:39:45 +0900

    build: nekonata_location_fetcher を SPM へ移行

--- packages/nekonata_location_fetcher/ios/nekonata_location_fetcher/Package.swift
+++ packages/nekonata_location_fetcher/ios/nekonata_location_fetcher/Package.swift
@@ -16,12 +16,13 @@ let package = Package(
         .target(
             name: "nekonata_location_fetcher",
             dependencies: [],
+            path: "Sources",
             resources: [
                 // If your plugin requires a privacy manifest, for example if it uses any required
                 // reason APIs, update the PrivacyInfo.xcprivacy file to describe your plugin's
                 // privacy impact, and then uncomment these lines. For more information, see
                 // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-                // .process("PrivacyInfo.xcprivacy"),
+                .process("PrivacyInfo.xcprivacy"),
 
                 // If you have other resources that need to be bundled with your plugin, refer to
                 // the following instructions to add them:
